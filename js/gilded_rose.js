5c5aea447e26cbbc2d84eb6a3a93934c8e9d0a72
Chunky <jasonc@nulogy.com>
2020-12-16 22:21:40 -0500

    refactor

--- js/gilded_rose.js
+++ js/gilded_rose.js
@@ -9,27 +9,31 @@ export class Shop {
   }
 
   updateItem(item) {
-    if (item.name === "Aged Brie") {
-      item.sellIn--;
-      item.quality += item.sellIn < 0 ? 2 : 1;
-      if (item.quality > 50) item.quality = 50;
-    } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-      item.sellIn--;
-      if (item.sellIn >= 10) {
-        item.quality += 1;
-      } else if (item.sellIn >= 5) {
-        item.quality += 2;
-      } else if (item.sellIn >= 0) {
-        item.quality += 3;
-      } else {
-        item.quality = 0;
-      }
-      if (item.quality > 50) item.quality = 50;
-    } else if (item.name === "Sulfuras, Hand of Ragnaros") {
-    } else {
-      item.sellIn--;
-      item.quality -= item.sellIn < 0 ? 2 : 1;
-      if (item.quality < 0) item.quality = 0;
+    switch (item.name) {
+      case "Aged Brie":
+        item.sellIn--;
+        item.quality += item.sellIn < 0 ? 2 : 1;
+        if (item.quality > 50) item.quality = 50;
+        break;
+      case "Backstage passes to a TAFKAL80ETC concert":
+        item.sellIn--;
+        if (item.sellIn >= 10) {
+          item.quality += 1;
+        } else if (item.sellIn >= 5) {
+          item.quality += 2;
+        } else if (item.sellIn >= 0) {
+          item.quality += 3;
+        } else {
+          item.quality = 0;
+        }
+        if (item.quality > 50) item.quality = 50;
+        break;
+      case "Sulfuras, Hand of Ragnaros":
+        break;
+      default:
+        item.sellIn--;
+        item.quality -= item.sellIn < 0 ? 2 : 1;
+        if (item.quality < 0) item.quality = 0;
     }
   }
 }
