9cc2e3c0c2abb1fb19194b56c346a2fb6237fbad
Chunky <jasonc@nulogy.com>
2021-06-03 05:38:22 -0400

    refactor

--- ruby/lib/item_types/backstage_passes.rb
+++ ruby/lib/item_types/backstage_passes.rb
@@ -8,14 +8,19 @@ module ItemTypes
 
     def update_item(item)
       item.sell_in -= 1
-      item.quality =
-        case item.sell_in
-        when (10..) then item.quality + 1
-        when (5..) then item.quality + 2
-        when (0..) then item.quality + 3
-        else 0
-        end
+      item.quality = next_quality(item)
       item.quality = item.quality.clamp(0, 50)
+    end
+
+    private
+
+    def next_quality(item)
+      case item.sell_in
+      when (10..) then item.quality + 1
+      when (5..) then item.quality + 2
+      when (0..) then item.quality + 3
+      else 0
+      end
     end
   end
 end
