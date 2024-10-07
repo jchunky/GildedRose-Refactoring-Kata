a6c16a57786ee2701552ec8b23b529e42ad90d3e
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-07 00:46:53 -0400

    refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,54 +1,52 @@
-class GildedRose
+class GildedRose < Struct.new(:items)
+  MIN_QUALITY = 0
+  MAX_QUALITY = 50
 
-  def initialize(items)
-    @items = items
+  def update_quality
+    items.each(&method(:process_item))
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+  private
+
+  def process_item(item)
+    return if item.name == "Sulfuras, Hand of Ragnaros"
+
+    update_sell_in_of(item)
+    update_quality_of(item)
+  end
+
+  def update_sell_in_of(item)
+    item.sell_in -= 1
+  end
+
+  def update_quality_of(item)
+    quality_change = calculate_quality_change(item)
+    item.quality = (item.quality + quality_change).clamp(MIN_QUALITY, MAX_QUALITY)
+  end
+
+  def calculate_quality_change(item)
+    case item.name
+    when "Aged Brie"
+      calculate_increment(item, 1)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      calculate_backstage_pass_increment(item)
+    else
+      calculate_increment(item, -1)
+    end
+  end
+
+  def calculate_increment(item, base_increment)
+    increment = base_increment
+    increment *= 2 if item.sell_in < 0
+    increment
+  end
+
+  def calculate_backstage_pass_increment(item)
+    case item.sell_in
+    when (10..) then 1
+    when (5..) then 2
+    when (0..) then 3
+    else -item.quality
     end
   end
 end
