997cc1ca4cbf2b79b9c199f5048e7e37c83465c7
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-16 09:57:14 -0400

    refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,54 +1,82 @@
 class GildedRose
-
   def initialize(items)
-    @items = items
+    @items = items.map { |item| ItemFactory.create(item) }
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+  def update_quality
+    @items.each(&:update_quality)
+  end
+end
+
+class ItemFactory
+  def self.create(item)
+    case item.name
+    when /Aged Brie/
+      AgedBrie.new(item)
+    when /Backstage pass/
+      BackstagePass.new(item)
+    when /Sulfuras/
+      LegendaryItem.new(item)
+    else
+      NormalItem.new(item)
     end
+  end
+end
+
+class ItemWrapper
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update_quality
+    raise NotImplementedError, "Subclasses must implement"
+  end
+
+  protected
+
+  def decrease_sell_in
+    @item.sell_in -= 1
+  end
+
+  def increase_quality
+    @item.quality += 1 if @item.quality < 50
+  end
+
+  def decrease_quality
+    @item.quality -= 1 if @item.quality > 0
+  end
+end
+
+class NormalItem < ItemWrapper
+  def update_quality
+    decrease_sell_in
+    decrease_quality
+    decrease_quality if @item.sell_in < 0
+  end
+end
+
+class AgedBrie < ItemWrapper
+  def update_quality
+    decrease_sell_in
+    increase_quality
+    increase_quality if @item.sell_in < 0
+  end
+end
+
+class BackstagePass < ItemWrapper
+  def update_quality
+    decrease_sell_in
+    increase_quality
+    increase_quality if @item.sell_in < 10
+    increase_quality if @item.sell_in < 5
+    @item.quality = 0 if @item.sell_in < 0
+  end
+end
+
+class LegendaryItem < ItemWrapper
+  def update_quality
+    # Legendary items do not change in quality or sell_in
   end
 end
