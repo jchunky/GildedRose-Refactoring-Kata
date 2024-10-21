c0baf6d5086bf02fa2a6534216693f46cd8e88d0
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-21 09:56:04 -0400

    refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -13,34 +13,28 @@ class GildedRose
     case item.name
     when /Aged Brie/
       item.sell_in -= 1
-
       item.quality += case item.sell_in
                       when ...0 then 2
                       else           1
                       end
-
       item.quality  = item.quality.clamp(0, 50)
     when /Backstage pass/
       item.sell_in -= 1
-
       item.quality += case item.sell_in
                       when ...0  then -item.quality
                       when ...5  then 3
                       when ...10 then 2
                       else            1
                       end
-
       item.quality  = item.quality.clamp(0, 50)
     when /Sulfuras/
       # no-op
     else
       item.sell_in -= 1
-
       item.quality += case item.sell_in
                       when ...0 then -2
                       else           -1
                       end
-
       item.quality = item.quality.clamp(0, 50)
     end
   end
