4cd12122777ffc90dacc58848d942602cfec5b4f
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-24 01:32:05 -0400

    refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -14,22 +14,22 @@ class GildedRose
     when /Aged Brie/
       item.sell_in -= 1
       item.quality += item.sell_in < 0 ? 2 : 1
-      item.quality  = item.quality.clamp(0, 50)
+      item.quality = item.quality.clamp(0, 50)
     when /Backstage pass/
       item.sell_in -= 1
       item.quality += case item.sell_in
                       when 10.. then 1
-                      when 5..  then 2
-                      when 0..  then 3
-                      else           -item.quality
+                      when 5.. then 2
+                      when 0.. then 3
+                      else -item.quality
                       end
-      item.quality  = item.quality.clamp(0, 50)
+      item.quality = item.quality.clamp(0, 50)
     when /Sulfuras/
       # no-op
     else
       item.sell_in -= 1
       item.quality += item.sell_in < 0 ? -2 : -1
-      item.quality  = item.quality.clamp(0, 50)
+      item.quality = item.quality.clamp(0, 50)
     end
   end
 end
--- ruby/gilded_rose_old.rb
+++ ruby/gilded_rose_old.rb
@@ -50,7 +50,7 @@ class Item
   attr_accessor :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
-    @name    = name
+    @name = name
     @sell_in = sell_in
     @quality = quality
   end
